b4c34b1032e2dcb8148dbe0bc8a5f6ba72fb4a49
ajn404 <nhy1423238345@163.com>
2021-11-29 11:15:47 +0800

    'template'

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import FadeInAnimation from "./FadeInAnimation";
 import './App.css';
 
-const words = "Hello darkness my old friend,i've come to talk with you again,because a version softly creeping,left it's seed while i was sleeping"
+const words = "Hello darkness my old friend," +
+    "i've come to talk with you again," +
+    "because a version softly creeping," +
+    "left it's seed while i was sleeping,And the vision that was planted in my brain," +
+    "Still remains within," +
+    "the sound of silence."
 let str = [];
 words.split(',').map((item, index) => {
   str.push(<FadeInAnimation direction="left" style={{
     fontWeight: "bold",
     fontSize: "20px"
-  }} delay={index} key={index}>{item}</FadeInAnimation>)
+  }} delay={index*1.5} key={index}>{item}</FadeInAnimation>)
   return str
 })
 export default function App() {
