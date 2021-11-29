1e4f65dd6e35320b98aec3c7d1b6fe26c9cf9e02
ajn404 <nhy1423238345@163.com>
2021-11-29 10:30:04 +0800

    'actions'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import FadeInAnimation from "./FadeInAnimation";
 import './App.css';
-const words="Hello darkness my old friend,i've come to talk with you again,because a version softly creeping,left it's seed while i was sleeping"
-let str=[];
-words.split(',').map((item,index)=>{
-str.push(<FadeInAnimation direction="left" style={{
-  fontWeight:"bold",
-  fontSize:"20px"
-}} delay={index} key={index}>{item}</FadeInAnimation>)
-return str
+
+const words = "Hello darkness my old friend,i've come to talk with you again,because a version softly creeping,left it's seed while i was sleeping"
+let str = [];
+words.split(',').map((item, index) => {
+  str.push(<FadeInAnimation direction="left" style={{
+    fontWeight: "bold",
+    fontSize: "20px"
+  }} delay={index} key={index}>{item}</FadeInAnimation>)
+  return str
 })
 export default function App() {
   return (
-    <div className='App'>
-      {str}
-    </div>
+      <div className='App'>
+        {str}
+      </div>
   );
 }
