43e0ef26a831d96334fc2111d6ad47ed48643c31
ajn404 <huiyuening00@gmail.com>
2021-12-10 00:27:25 +0800

    路由待使用

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import {BrowserRouter, Route, Switch, Routes} from "react-router-dom";
+import routers from "./router/index";
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from './App'
+
+
+ReactDOM.render (
+            // routers.map(router => {
+            //   <Routes>
+            //     return (
+            //         <Route path={
+            //                 router.path
+            //             }
+            //             component={
+            //                 router.component
+            //         }></Route>
+            //     )
+            //     </Routes>
+            // })
+            <App></App>
+        ,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
