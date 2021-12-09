881c2262d4a20d22311144596600381630c8e791
ajn404 <huiyuening00@gmail.com>
2021-12-09 23:13:21 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -4,24 +4,6 @@ import FadeInAnimation from "./FadeInAnimation";
 export default function App() {
   return (
     <div>
-      {/* <FadeInAnimation wrapperElement="h1" direction="down">
-        Hello CodeSandbox
-      </FadeInAnimation>
-      <FadeInAnimation wrapperElement="h2" direction="right" delay={1}>
-        Start editing to see some magic happen!
-      </FadeInAnimation>
-      <FadeInAnimation
-        style={{
-          width: 200,
-          color: "white",
-          height: 200,
-          backgroundColor: "purple",
-        }}
-        direction="up"
-      >
-
-        <p>Hello</p>
-      </FadeInAnimation> */}
       <FadeInAnimation direction="left" style={{
         fontWeight:"bold"
       }} delay="1">
